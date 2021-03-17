9e2869463a3f125b9ac831a1b58bf36646e16823
Mateus Gomes <mtsgomesp@gmail.com>
2021-03-16 22:17:53 -0300

    feat: index.ejs

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require("dotenv-safe").config();
+const jwt = require('jsonwebtoken');
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
