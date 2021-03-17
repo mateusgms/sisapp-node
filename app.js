7d611c3435a3a238a18cfd100f5de1d6fc40e95b
Mateus Gomes <mtsgomesp@gmail.com>
2021-03-16 23:18:51 -0300

    Fix: cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 
 require("dotenv").config();
@@ -11,12 +12,13 @@ require("dotenv").config();
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
+const estipulantesRouter = require('./routes/estipulantes');
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,7 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/login', loginRouter)
+app.use('/login', loginRouter);
+app.use('/estipulantes', estipulantesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
